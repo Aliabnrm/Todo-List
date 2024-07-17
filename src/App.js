134ebd5a8bed408c93f95de3abeb470f37cc808b
Ali <aliabnar211@gmail.com>
2024-07-17 12:02:14 -0700

    init repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import swal from "sweetalert";
 
-function App() {
+import "./App.css";
+import Todo from "./Components/Todo";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
+  useEffect(() => {
+    getAllTodos();
+  }, []);
+
+  function getAllTodos() {
+    fetch("http://localhost:4000/todos")
+      .then((res) => res.json())
+      .then((datas) => setTodos(datas));
+  }
+
+  const todoRemoveHandler = (id) => {
+    fetch(`http://localhost:4000/todos/${id}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.status === 200) {
+        swal({
+          title: "Todo Removed Successfully ;))",
+          icon: "success",
+          buttons: "Hmmm, Ok",
+        });
+        getAllTodos();
+      }
+    });
+  };
+
+  const todoDoneHandler = (todo) => {
+    fetch(`http://localhost:4000/todos/${todo.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...todo,
+        isDone: !todo.isDone,
+      }),
+    }).then((res) => {
+      if (res.status === 200) {
+        swal({
+          title: "Todo Done Successfully ;))",
+          icon: "success",
+          buttons: "Hmmm. Ok",
+        });
+      }
+      getAllTodos();
+    });
+  };
+
+  const createTodoHandler = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:4000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: newTodoTitle,
+        isDone: false,
+      }),
+    }).then((res) => {
+      if (res.status === 201) {
+        swal({
+          title: "Todo Created Successfully ;))",
+          icon: "success",
+          buttons: "Hmmm, Ok",
+        });
+        setNewTodoTitle("");
+        getAllTodos();
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1> Todo List</h1>
       </header>
-    </div>
+      <form action="">
+        <input
+          type="text"
+          className="todo-input"
+          value={newTodoTitle}
+          onChange={(event) => setNewTodoTitle(event.target.value)}
+        />
+        <button
+          className="todo-button"
+          type="submit"
+          onClick={createTodoHandler}
+        >
+          <i className="fas fa-plus-circle fa-lg"></i>
+        </button>
+        <div className="select">
+          <select name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="incomplete">Incomplete</option>
+          </select>
+        </div>
+      </form>
+
+      <div className="todo-container">
+        <ul className="todo-list">
+          <Todo
+            todos={todos}
+            onRemove={todoRemoveHandler}
+            onDo={todoDoneHandler}
+          />
+        </ul>
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/Components/Todo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Todo({ todos, onRemove, onDo }) {
+  return (
+    <>
+      {todos.map((todo) => (
+        <div key={todo.id} className={`todo ${todo.isDone ? 'completed' : ''}`}>
+          <li className="todo-item">{todo.title}</li>
+          <button className="complete-btn" onClick={() => onDo(todo)}>
+            <i className="fas fa-check-circle"></i>
+          </button>
+          <button className="trash-btn" onClick={() => onRemove(todo.id)}>
+            <i className="fas fa-trash"></i>
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
